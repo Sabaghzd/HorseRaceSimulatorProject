ce44b594aa7f7bf7ac7b6694381bcec5eb1bec2e
Sabaghzd <98692878+Sabaghzd@users.noreply.github.com>
2024-04-26 13:48:29 +0100

    Main method added for testing

---
+++ Part1/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Race race = new Race(20, 4);
+
+        Horse horse1 = new Horse('\u265E', "Harry", 0.8);
+        Horse horse2 = new Horse('\uD83C\uDFC7', "Max", 0.6);
+        Horse horse3 = new Horse('\uD83D\uDC0E', "Sam", 0.7);
+        Horse horse4 = new Horse('\uD83D\uDC34', "Karen", 0.7);
+
+
+        race.addHorse(horse1, 1);
+        race.addHorse(horse2, 2);
+        race.addHorse(horse3, 3);
+        race.addHorse(horse4, 4);
+
+
+
+        race.startRace();
+    }
+}
