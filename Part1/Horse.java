217389efba449e421218148a98a5cfde327ee00a
Sabaghzd <98692878+Sabaghzd@users.noreply.github.com>
2024-04-26 02:17:44 +0100

    Updated Horse Class with working methods

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -1,76 +1,100 @@
 
 /**
- * Write a description of class Horse here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ *
+ * The Horse class models individual racehorses, tracking attributes like
+ * symbol, name, confidence, distance travelled, and whether they've fallen.
+ * It provides methods to adjust confidence, move forward, and manage race progress.
+ *
+ * @author Saba Ghasemzadeh
+ * @version final
  */
 public class Horse
 {
-    //Fields of class Horse
-    
-    
-      
-    //Constructor of class Horse
+    //Fields of class Part1.Horse
+    char horseSymbol;
+    String horseName;
+    double horseConfidence;
+    int distanceTravelled;
+    boolean hasFallen;
+
+
+    //Constructor of class Part1.Horse
     /**
-     * Constructor for objects of class Horse
+     * Constructor for objects of class Part1.Horse
      */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
-       
+        this.horseSymbol = horseSymbol;
+        this.horseName = horseName;
+        this.horseConfidence = horseConfidence;
+
     }
-    
-    
-    
-    //Other methods of class Horse
+
+    //Other methods of class Part1.Horse
     public void fall()
     {
-        
+        this.hasFallen = true;
     }
-    
+
     public double getConfidence()
     {
-        
+        return this.horseConfidence;
+
     }
-    
+
+    // Method to adjust confidence during a race
+    public void adjustConfidence(boolean won) {
+        if (won) {
+            horseConfidence += 0.05; // Increase confidence slightly if won
+            if (horseConfidence > 1.0) {
+                horseConfidence = 1.0; // Ensure confidence doesn't exceed 1.0
+            }
+        } else {
+            horseConfidence -= 0.05; // Decrease confidence slightly if fell
+            if (horseConfidence < 0.0) {
+                horseConfidence = 0.0; // Ensure confidence doesn't go below 0.0
+            }
+        }
+    }
+
     public int getDistanceTravelled()
     {
-        
+        return this.distanceTravelled;
     }
-    
+
     public String getName()
     {
-        
+        return this.horseName;
     }
-    
+
     public char getSymbol()
     {
-        
+        return this.horseSymbol;
     }
-    
+
     public void goBackToStart()
     {
-        
+        this.distanceTravelled = 0;
     }
-    
+
     public boolean hasFallen()
     {
-        
+        return hasFallen;
     }
 
     public void moveForward()
     {
-        
+        this.distanceTravelled++;
     }
 
     public void setConfidence(double newConfidence)
     {
-        
+        this.horseConfidence = newConfidence;
     }
-    
+
     public void setSymbol(char newSymbol)
     {
-        
+        this.horseSymbol = newSymbol;
     }
-    
+
 }
